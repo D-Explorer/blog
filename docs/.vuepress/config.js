3dc06a6c132db80b7c1b85c58ed01ea66b0f27d8
起不了床的夜猫子 <d_explorer@163.com>
2020-10-03 22:58:07 +0800

    再次测试网站title icon  问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,13 @@ module.exports = {
     title: '起不了床的夜猫子',
     description: '起不了床的夜猫子的 VuePress 文档',
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }] //favicon.ico 图标要放在 pulic 文件夹下面
-      ],
+      ['link', { rel: 'icon', href: '/favicon.ico' }],
+      ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+     ],
     plugins: pluginsConf,
     themeConfig: {
         lastUpdated: '上次更新',
+        smoothScroll: true,
         nav: navConf,
     },
 
